1f6c56e3d35f2ef8fc7f10089f87dd5fa6769793
YeonGu <1277941818@qq.com>
2024-03-11 19:50:59 +0800

    chap 2

--- others/科协内部嵌入式参考/docs/a.cpp
+++ others/科协内部嵌入式参考/docs/a.cpp
@@ -1,7 +1,23 @@
-switch (state) {
-  CASE(0, ONELT(0));
-  CASE(3, ONELT(1));
-  CASE(4, ONELT(2));
-default:
-  break;
+#define GPIOB_BASE 0x ? ? ? ? ? ? ? ?         // To be implemented...
+#define GPIOx_ODR_OFFSET 0x ? ? ? ? ? ? ? ? ? // To be implemented...
+#define GET_ADDR(base, offset) (base + offset)
+#define GPIOB_REG(reg) GET_ADDR(GPIOB_BASE, GPIOx_##reg##_OFFSET)
+
+/**
+ * @brief GPIOB的PBn引脚置位
+ */
+void gpiob_set(uint8_t n) {
+  // To be implemented...
+}
+/**
+ * @brief GPIOB的PBn引脚复位
+ */
+void gpiob_reset(uint8_t n) {
+  // To be implemented...
+}
+/**
+ * @brief GPIOB的PBn引脚写电平, 如果val==0则复位, 否则置位
+ */
+void gpiob_write(uint8_t n, uint8_t val) {
+  // To be implemented...
 }
